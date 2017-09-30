d52ca390af59912f5b7fc6e64baede720725d8da
ryan0122 <ryan.mack1200@gmail.com>
2017-09-30 13:18:02 -0600

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  // socket.emit from Admin text Welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  // socket.broadcast.emit, from Admin text New user joined
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (msg) => {
     console.log(msg);
 
-    io.emit('newMessage', {
+    socket.broadcast.emit('newMessage', {
       from: msg.from,
       text: msg.text,
       createdAt: new Date().getTime()
     });
+
+    //socket.broadcast.emit('newMessage', {
+    //  from: msg.from,
+    //  text: msg.text
+    //});
   });
 
   socket.on('disconnect', () => {
